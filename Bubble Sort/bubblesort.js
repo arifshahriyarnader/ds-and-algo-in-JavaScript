633ce10753802860cde9e5c90a176bff660e4efe
Arif Shahriyar Nader <arif.nader95@gmail.com>
2021-11-28 00:46:45 +0600

    Bubble Sort

---
+++ Bubble Sort/bubblesort.js
@@ -0,0 +1,18 @@
+const bubbleSort = (inputArray) =>{
+    let n = inputArray.length;
+    let i, j,temp;
+    for(i =0; i<n; i++){
+        for(j=0; j < n-1; j++){
+            //Swapping the Elements
+            if(inputArray[j] > inputArray[j+1]){
+                temp = inputArray[j];
+                inputArray[j] = inputArray[j+1];
+                inputArray[j+1] = temp;
+            }
+
+        }
+    }
+}
+const inputArray = [5,2,8,6,3,9,15,10];
+bubbleSort(inputArray);
+console.log(inputArray);
