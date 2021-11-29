cbdc42919abee563744ba8ac271291e240720bfe
Arif Shahriyar Nader <arif.nader95@gmail.com>
2021-11-29 12:39:29 +0600

    Insertion Sort

--- Insertion Sort/insertionsort.js
+++ Insertion Sort/insertionsort.js
@@ -1,17 +1,17 @@
 const insertionSort = (inputArray) =>{
     let n = inputArray.length;
-    let i, j, item;
-    for(i =1; i<n; i++){
-         item = inputArray[i];
-         j = i-1;
-         while(j >= 0 && inputArray[j] > item){
-             inputArray[j+1] = inputArray[j];
-             j = j-1;
-         }
-         inputArray[j+1] = item;
-    }  
+    let i,j,item;
+    for(i=1; i<n; i++){
+        item = inputArray[i];
+        j = i-1;
+        while(j >=0 && inputArray[j] > item){
+            inputArray[j+1] = inputArray[j];
+            j = j-1;
+        }
+        inputArray[j+1] = item;
+    }
     return inputArray;
 }
-const inputArray=[3,44,38,2,45,42,48,47,49,40,50];
+const inputArray = [4,2,3,5,9,7,8,6,15,12,20,18];
 insertionSort(inputArray);
 console.log(inputArray);
